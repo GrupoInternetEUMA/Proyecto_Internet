95e2dbdc55ae088fbf1f05830b9d91a090d01029
Antonio Cabañas <antoniozurita99@gmail.com>
2020-04-27 18:38:11 +0200

    Creado Proyecto en NETBEANS_Funciona a la perfección todo lo editado hasta ahora

---
+++ ProyectoAPS/src/java/es/uma/informatica/sii/jsf/autenticacion/ControlAutorizacion.java
@@ -0,0 +1,92 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.uma.informatica.sii.jsf.autenticacion;
+
+import es.uma.informatica.sii.acoes.Usuario;
+import static es.uma.informatica.sii.acoes.Usuario.Rol.ADMINISTRADOR;
+import static es.uma.informatica.sii.acoes.Usuario.Rol.ONG;
+import static es.uma.informatica.sii.acoes.Usuario.Rol.RESPONSABLE;
+import static es.uma.informatica.sii.acoes.Usuario.Rol.ALUMNO;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author Jose Antonio Bravo Gonzalez
+ */
+@Named(value = "controlAutorizacion")
+@SessionScoped
+public class ControlAutorizacion implements Serializable {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Usuario usuario;
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public String home() {
+        if(getUsuario()==null){
+            return "login.xhtml";
+        }
+        
+        if(getUsuario().getRol().equals(getUsuario().getRol().ADMINISTRADOR)){
+            return "admin.xhtml";
+        }
+        
+        if(getUsuario().getRol().equals(getUsuario().getRol().ONG)){
+            return "ong.xhtml";
+        }
+        
+        if(getUsuario().getRol().equals(getUsuario().getRol().RESPONSABLE)){
+            return "responsable.xhtml";
+        }
+        
+        if(getUsuario().getRol().equals(getUsuario().getRol().ALUMNO)){
+            return "alumno.xhtml";
+        }
+        
+        return null;
+    }
+    
+    public String registro() {
+        if(getUsuario()==null){
+            return "registrarse.xhtml";
+        }
+        return null;
+    }
+    
+    public String listaONGs() {
+        if(getUsuario().getRol().equals(getUsuario().getRol().ADMINISTRADOR)){
+            return "listaONGs.xhtml";
+        }
+        return null;
+    }
+    
+    public String logout()
+    {
+        // Destruye la sesión (y con ello, el ámbito de este bean)
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        ctx.getExternalContext().invalidateSession();
+        usuario = null;
+        return "login.xhtml";
+    }
+
+    /**
+     * Creates a new instance of ControlAutorizacion
+     */
+    public ControlAutorizacion() {
+    }
+}
---
+++ ProyectoAPS/src/java/es/uma/informatica/sii/jsf/autenticacion/Login.java
@@ -0,0 +1,99 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.uma.informatica.sii.jsf.autenticacion;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import  es.uma.informatica.sii.acoes.Usuario;
+import  es.uma.informatica.sii.acoes.Usuario.Rol;
+import es.uma.informatica.sii.jsf.autenticacion.ControlAutorizacion;
+
+/**
+ *
+ * @author Jose Antonio Bravo Gonzalez
+ */
+@Named(value = "login")
+@RequestScoped
+public class Login {
+
+    private String usuario;
+    private String contrasenia;
+    private List<Usuario> usuarios;
+    
+    @Inject
+    private ControlAutorizacion ctrl;
+
+    /**
+     * Creates a new instance of Login
+     */
+    public Login() {
+        usuarios = new ArrayList<Usuario>();
+        usuarios.add(new Usuario("admin", "admin", Rol.ADMINISTRADOR));
+        usuarios.add(new Usuario("ong", "ong", Rol.ONG));
+        usuarios.add(new Usuario("resp", "resp", Rol.RESPONSABLE));
+        usuarios.add(new Usuario("alu", "alu", Rol.ALUMNO));
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String autenticar() {
+        // Implementar este método
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        Iterator<Usuario> iter = usuarios.iterator();
+        boolean pass = false;
+        boolean user = false;
+        Usuario usuario = null;
+        
+        while(iter.hasNext() && !user){
+            usuario = iter.next();
+            
+            if(usuario.getUsuario().equals(getUsuario())){
+                user = true;
+                if(usuario.getContrasenia().equals(getContrasenia())){
+                    pass = true;
+                    ctrl.setUsuario(usuario);
+                    return ctrl.home();
+                }
+            }
+        }
+        
+        if(!user){
+            ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"El usuario introducido no existe", "El usuario introducido no existe"));
+            return null;
+        }
+        
+        if(!pass){
+            ctx.addMessage(null,new FacesMessage(FacesMessage.SEVERITY_ERROR,"La contraseña introducida es incorrecta", "La contraseña introducida es incorrecta"));
+            return null;
+        }
+        
+        ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "El método autenticar() no está implementado", "El método autenticar() no está implementado"));
+        return null;
+    }
+}
+
+
