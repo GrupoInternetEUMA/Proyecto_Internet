cd94022a76479ff0f2f3b5bcc66a28ef3acee537
Antonio Cabañas <antoniozurita99@gmail.com>
2020-04-28 14:31:38 +0200

    Revert "modificar solicitud"
    This reverts commit a9e1a891e46ee73cba573d7c324567206c2a28a1.

--- ProyectoAPS/src/java/es/uma/informatica/sii/acoes/ModificarSolicitud.java
+++ ProyectoAPS/src/java/es/uma/informatica/sii/acoes/ModificarSolicitud.java
@@ -3,9 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package es.uma.informatica.sii.jsf.autenticacion;
+package es.uma.informatica.sii.acoes;
 
-import es.uma.informatica.sii.acoes.Solicitud_Actividad;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
