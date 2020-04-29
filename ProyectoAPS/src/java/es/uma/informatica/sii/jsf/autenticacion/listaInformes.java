688457353f26fd71f9dbdd9a79bf98094057f83c
jbg1996 <jbg1996@users.noreply.github.com>
2020-04-29 02:18:23 +0200

    123

--- ProyectoAPS/src/java/es/uma/informatica/sii/jsf/autenticacion/listaInformes.java
+++ ProyectoAPS/src/java/es/uma/informatica/sii/jsf/autenticacion/listaInformes.java
@@ -11,32 +11,32 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import es.uma.informatica.sii.acoes.*;
 
-
-
 /**
  *
  * @author Antonio
  */
-
 @Named(value = "ListaInformes")
 @SessionScoped
 public class listaInformes implements Serializable {
+
     private final ArrayList<Informe> informes;
     private Actividad actividad;
     private Usuario usuario;
     private Informe informe;
 
-    public listaInformes(){
+    public listaInformes() {
         informes = new ArrayList<>();
-        informes.add(new Informe(1,"Obtención de 5 créditos","180 horas","Curso de montaje de ordenador"));
-        informes.add(new Informe(2,"Obtención de 1 créditos","30 horas","Tareas de mantenimiento de jardinería"));
-        informes.add(new Informe(3,"Obtención de 2 créditos","50 horas","Informes de alumno ERASMUS"));
-        informes.add(new Informe(4,"Obtención de 6 créditos","200 horas","Investigación en laboratorio"));
+        informes.add(new Informe(1, "Obtención de 5 créditos", "180 horas", "Curso de montaje de ordenador"));
+        informes.add(new Informe(2, "Obtención de 1 créditos", "30 horas", "Tareas de mantenimiento de jardinería"));
+        informes.add(new Informe(3, "Obtención de 2 créditos", "50 horas", "Informes de alumno ERASMUS"));
+        informes.add(new Informe(4, "Obtención de 6 créditos", "200 horas", "Investigación en laboratorio"));
         informe = new Informe();
     }
-    public ArrayList<Informe> getInformes(){
+
+    public ArrayList<Informe> getInformes() {
         return informes;
     }
+
     public Actividad getActividad() {
         return actividad;
     }
@@ -61,8 +61,8 @@ public class listaInformes implements Serializable {
         this.informe = informe;
     }
 
-    public String crearInforme(){
-        return "insertarInforme.xhtml";
+    public String crearInforme() {
+        return "anadirInforme.xhtml";
     }
 
 }
