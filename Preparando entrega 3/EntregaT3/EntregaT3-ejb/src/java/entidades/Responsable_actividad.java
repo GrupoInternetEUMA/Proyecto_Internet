6f8b666585c3e91063424ab21896c74c08968f98
Enriquelp <48442517+Enriquelp@users.noreply.github.com>
2020-05-26 21:13:45 +0200

    Empezando entrega 3

---
+++ Preparando entrega 3/EntregaT3/EntregaT3-ejb/src/java/entidades/Responsable_actividad.java
@@ -0,0 +1,88 @@
+package entidades;
+
+import entidades.Usuario;
+import java.util.Date;
+import java.io.Serializable;
+import java.lang.String;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: responsable
+ *
+ */
+@Entity
+
+public class Responsable_actividad extends Usuario implements Serializable {
+
+    private String departamento;
+    @ManyToMany
+    private Collection<Usuario> usuarios;
+
+    @OneToMany(mappedBy = "responsable_actividad")
+    private Collection<Actividad> actividades;
+    private static final long serialVersionUID = 1L;
+
+    public Responsable_actividad() {
+        super();
+    }
+
+    public Responsable_actividad(Integer id, Integer dni, String nombre, String apellidos, String estudios, String idioma, Date fecha_nacimiento, String email, String contrasenia, Rol rol, String usuario, String departamento) {
+        super(id, dni, nombre, apellidos, estudios, idioma, fecha_nacimiento, email, contrasenia, rol, usuario);
+        this.departamento = departamento;
+    }
+
+    public String getDepartamento() {
+        return this.departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((actividades == null) ? 0 : actividades.hashCode());
+        result = prime * result + ((departamento == null) ? 0 : departamento.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Responsable_actividad other = (Responsable_actividad) obj;
+        if (actividades == null) {
+            if (other.actividades != null) {
+                return false;
+            }
+        } else if (!actividades.equals(other.actividades)) {
+            return false;
+        }
+        if (departamento == null) {
+            if (other.departamento != null) {
+                return false;
+            }
+        } else if (!departamento.equals(other.departamento)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Responsable_actividad [departamento=" + departamento + ", Id: " + getId() + ", Nombre: "
+                + getNombre() + ", Apellidos: " + getApellidos() + " Rol: " + getRol() + "]";
+    }
+
+}
