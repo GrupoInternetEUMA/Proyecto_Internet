938e6d216346f3baab268c5cef47d513d1b75e56
Enriquelp <48442517+Enriquelp@users.noreply.github.com>
2020-05-31 19:42:18 +0200

    algunos metodos para ir porbando

--- Preparando entrega 3/APS0/APS0-ejb/src/java/Aplicacion/BaseDeDatos.java
+++ Preparando entrega 3/APS0/APS0-ejb/src/java/Aplicacion/BaseDeDatos.java
@@ -25,7 +25,7 @@ public class BaseDeDatos implements BaseDeDatosLocal {
     @PersistenceContext(unitName = "APS0-ejbPU")
     private EntityManager em;
     
-  public List<Alumno> getAlumnos(){
+  public List<Alumno> getAlumnos1(){
       ArrayList<Alumno> alumnos = new ArrayList<Alumno>();
       alumnos.add(new Alumno( 12345678, "David", "El Gnomo", null, "Castellano", new Date(1997, 2, 28), "prueba@uma.es", "1234", Rol.ALUMNO, "DavidGnomo", "Ing Informatica", 1231234));
       alumnos.add(new Alumno( 12312312, "Anastasia", "Sanches Cotrina", "Psicologia", "Castellano, Ingles", new Date(1998, 11, 3), "La_Anastasita_xula@uma.es", "twerk", Rol.ALUMNO, "AnaSans", "Universidad de la calle", 1234729));
@@ -34,8 +34,37 @@ public class BaseDeDatos implements BaseDeDatosLocal {
       return alumnos;
   }
   
-  public List<Alumno> getAlumnos1(){
+  public List<Alumno> getAlumnos(){
       Query query = em.createQuery("Select a from Alumno a");
       return query.getResultList();
+  }
+  
+  public List<Alumno> getResponsables(){
+      Query query = em.createQuery("Select a from Responsable_actividad a");
+      return query.getResultList();
+  }
+  
+  public List<Alumno> getONGs(){
+      Query query = em.createQuery("Select a from ONG a");
+      return query.getResultList();
+  }
+  
+  public List<Alumno> getUsuarios(){
+      Query query = em.createQuery("Select a from Usuario a");
+      return query.getResultList();
+  }
+  
+  public List<Alumno> getActividades(){
+      Query query = em.createQuery("Select a from Actividad a");
+      return query.getResultList();
+  }
+  
+  public List<Alumno> getInformes(){
+      Query query = em.createQuery("Select a from Informe a");
+      return query.getResultList();
+  }
+  
+  public void insertarAlummno(){
+      
   }
 }
