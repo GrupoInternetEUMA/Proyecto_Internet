43df3b903e5c524ab42e0584c68bc758d1cbcc0f
Antonio Cabañas <antoniozurita99@gmail.com>
2020-06-04 19:24:38 +0200

    Revert "comit erroneo"
    This reverts commit 5856fae1f561a6f754f210aec4baaa7d73395274.

--- Preparando entrega 3/APSNUEVO/APS0/APS0-ejb/build/generated-sources/ap-source-output/Entidades/Actividad_.java
+++ Preparando entrega 3/APSNUEVO/APS0/APS0-ejb/build/generated-sources/ap-source-output/Entidades/Actividad_.java
@@ -10,7 +10,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2020-06-04T19:22:27")
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2020-06-04T18:39:51")
 @StaticMetamodel(Actividad.class)
 public class Actividad_ { 
 
--- Preparando entrega 3/APSNUEVO/APS0/dist/gfdeploy/APS0/APS0-ejb_jar/resources/data.sql
+++ Preparando entrega 3/APSNUEVO/APS0/dist/gfdeploy/APS0/APS0-ejb_jar/resources/data.sql
@@ -7,10 +7,44 @@
  * Author:  Antonio
  * Created: 27-may-2020
  */
+INSERT INTO APP.USUARIO  VALUES ('Anastasia', 'Sanches','1234',50627756,'anas@gmail.com', 'Psicologia',  '2000-05-15', 'Español e ingles', 'anstasia', 'ALUMNO')
+
+INSERT INTO APP.USUARIO  (USUARIO, APELLIDOS, CONTRASENIA, DNI, EMAIL, ESTUDIOS, FECHA_NACIMIENTO, IDIOMA, NOMBRE, ROL) VALUES ('Detective', 'Holmes', 'pass12341', 77453728, 'usuario1@hotmail.com', 'psicologia', '1800-11-30','ingles', 'Sherlock', 'NORMAL')
+
+INSERT INTO APP.USUARIO  VALUES ('Actriz', 'Miller', 'contra12341', 77453756, 'usuario2@hotmail.com', 'psicologia','1800-11-30', 'ingles', 'Amarna', 'NORMAL')
+INSERT INTO APP.ALUMNO  VALUES ('Anastasia', 'Sanches','1234',50627756,'anas@gmail.com', 'Psicologia',  '2000-05-15', 'Español e ingles', 'anstasia', 123456, 'ALUMNO', 'Universidad de la calle')
+INSERT INTO APP.ALUMNO  VALUES ('Anastasio', 'martines','1234',40627756,'anaso@gmail.com', 'Mates',  '2000-05-23', 'Español e ingles', 'anstasi0', 1234567, 'ALUMNO', 'Universidad de la calle')
+
+INSERT INTO APP.USUARIO  VALUES ('Anastasia', 'Sanches','1234',50627756,'anas@gmail.com', 'Psicologia',  '2000-05-15', 'Español e ingles', 'anstasia', 'ALUMNO')
+INSERT INTO APP.USUARIO (USUARIO, APELLIDOS, CONTRASENIA, DNI, EMAIL, ESTUDIOS, FECHA_NACIMIENTO, IDIOMA, NOMBRE, ROL) VALUES ('admin', 'admin','admin',1,'admin@gmail.com', 'NULL',  '2000-05-15', 'NULL', 'admin', 'ADMINISTRADOR')
+
+INSERT INTO APP.RESPONSABLE_ACTIVIDAD VALUES ('CIENTIFICO1','SUAREZ','1234','LCC',88779944,'suarez@uma.es','Doctor Ingenería Informática','1970-01-22','Espanol e Ingles','Alfredo','RESPONSABLE')
+INSERT INTO APP.RESPONSABLE_ACTIVIDAD VALUES ('resp','SUAREZ','resp','LCC',88779944,'suarez@uma.es','Doctor Ingenería Informática','1970-01-22','Espanol e Ingles','Alfredo','RESPONSABLE')
+INSERT INTO APP.USUARIO VALUES ('resp','SUAREZ','resp',88779944,'suarez@uma.es','Doctor Ingenería Informática','1970-01-22','Espanol e Ingles','Alfredo','RESPONSABLE')
+
+INSERT INTO APP.ONG (USUARIO,CONTRASENIA, DESCRIPCION,DIRECCION,DNI, EMAIL, FECHA_NACIMIENTO,NOMBRE,PAIS, ROL,TELEFONO) VALUES ('ong3', '1234','ONG que trata de salvar el medio ambiente','Calle Picos de Europa',20572,'arbolesverdes@gmail.com','1999-08-15', 'Arboles verdes',  'España','ONG', 608321654)
+INSERT INTO APP.ONG (USUARIO,CONTRASENIA, DESCRIPCION,DIRECCION,DNI, EMAIL, FECHA_NACIMIENTO,NOMBRE,PAIS, ROL,TELEFONO) VALUES ('ong1', '1234','ONG Siempre acompañado es una ONG que se dedica a pasar el tiempo con aquellos que por cualquier circunstancia pasan mucho tiempo en soledad','Calle sin nombre',10225,'ONGSA@gmail.com','2000-01-20', 'Siempre acompañado',  'España','ONG', 661543216)
+INSERT INTO APP.ONG (USUARIO,CONTRASENIA, DESCRIPCION,DIRECCION,DNI, EMAIL, FECHA_NACIMIENTO,NOMBRE,PAIS, ROL,TELEFONO) VALUES ('ong2', '1234','ONG que trata de repartir recursos','Calle Quevedo',38504,'dosong@gmail.com','2004-03-19', 'Todos Tienen',  'España','ONG', 623630415)
 
 INSERT INTO APP.ACTIVIDAD VALUES(1,'Recogida de caca de perro','PENDIENTE','2020-05-12','2021-05-13','EN UN LUGAR DE LA MANCHA', 'RECOGIDA DE MIERDAS','VOLUNTARIADO','ong1','CIENTIFICO1' )
-
+INSERT INTO APP.USUARIO  VALUES ('Anastasia', 'Sanches','1234',50627756,'anas@gmail.com', 'Psicologia',  '2000-05-15', 'Español e ingles', 'anstasia', 'ALUMNO')
 INSERT INTO APP.INFORME  VALUES (1, 'REALIZADO CORRECTAMENTE','LIMPIEZA, AYUDA A NIÑOS','30 HORAS',1, 'Anastasia')
+INSERT INTO APP.USUARIO (USUARIO, APELLIDOS, CONTRASENIA, DNI, EMAIL, ESTUDIOS, FECHA_NACIMIENTO, IDIOMA, NOMBRE, ROL) VALUES ('Detective', 'Holmes', 'pass12341', 77453728, 'usuario1@hotmail.com', 'psicologia', '1800-11-30', 'ingles', 'Sherlock', 'USUARIO')
+
+INSERT INTO APP.USUARIO (USUARIO, APELLIDOS, CONTRASENIA, DNI, EMAIL, ESTUDIOS, FECHA_NACIMIENTO, IDIOMA, NOMBRE, ROL) VALUES ('Actriz', 'Miller', 'contra12341', 77453756, 'usuario2@hotmail.com', 'psicologia', '1993-10-30', 'ingles', 'Amarna', 'USUARIO')
+
+INSERT INTO APP.ALUMNO  VALUES ('Anastasia', 'Sanches','1234',50627756,'anas@gmail.com', 'Psicologia',  '2000-05-15', 'Español e ingles', 'anstasia', 123456, 'ALUMNO', 'Universidad de la calle')
+
+INSERT INTO APP.ALUMNO  VALUES ('Anastasio', 'martines','1234',40627756,'anaso@gmail.com', 'Mates',  '2000-05-23', 'Español e ingles', 'anstasi0', 1234567, 'ALUMNO', 'Universidad de la calle')
+
+INSERT INTO APP.ALUMNO (USUARIO, APELLIDOS, CONTRASENIA, DNI, EMAIL, ESTUDIOS, FECHA_NACIMIENTO, IDIOMA, NOMBRE, NUM_EXPEDIENTE, ROL, TITULACION) VALUES ('Filosofo', 'Gutierrez', 'pass6969', 77479828, 'alumno1@hotmail.com', 'Filosofia', '1990-11-21', 'ingles', 'Martin', 84938, 'ALUMNO', 'Ing. Informatica')
+
+INSERT INTO APP.ALUMNO (USUARIO, APELLIDOS, CONTRASENIA, DNI, EMAIL, ESTUDIOS, FECHA_NACIMIENTO, IDIOMA, NOMBRE, NUM_EXPEDIENTE, ROL, TITULACION) VALUES ('Reportera', 'Velasco', 'password', 7712358, 'alumno2@hotmail.com', 'Comunnicaciones', '2001-09-05', 'ingles', 'Monica', 54995, 'ALUMNO', 'Audiovisuales')
+
+
+INSERT INTO APP.RESPONSABLE_ACTIVIDAD VALUES ('CIENTIFICO1','SUAREZ','1234','LCC',88779944,'suarez@uma.es','Doctor Ingenería Informática','1970-01-22', 'Espanol e Ingles','Alfredo','RESPONSABLE')
+
+INSERT INTO APP.RESPONSABLE_ACTIVIDAD (USUARIO, APELLIDOS, CONTRASENIA, DEPARTAMENTO, DNI, EMAIL, ESTUDIOS, FECHA_NACIMIENTO, IDIOMA, NOMBRE, ROL) VALUES ('INVESTIGADOR1','Estrella','1234','LCC',11229944,'suarez@uma.es','Doctor biologia marina','1970-01-22','Espanol e Ingles','Bob','RESPONSABLE')
 
 INSERT INTO APP.ACTIVIDAD VALUES (1,'Recogida de caca de perro','PENDIENTE','2020-05-12','2021-05-13','EN UN LUGAR DE LA MANCHA', 'RECOGIDA DE MIERDAS','VOLUNTARIADO',1, 'CIENTIFICO1')
 
@@ -21,28 +55,11 @@ INSERT INTO APP.SOLICITUD_ACTIVIDAD (ID, ESTADO, FECHA_SOLICITUD, ACTIVIDAD_ID,U
 INSERT INTO APP.INFORME  VALUES (1, 'REALIZADO CORRECTAMENTE','LIMPIEZA, AYUDA A NIÑOS','30 HORAS',1, 'Anastasia')
 
 INSERT INTO APP.INFORME (ID, EVALUACION, TAREAS_REALIZADAS, TIEMPO_EMPLEADO, ACTIVIDAD_ID, USUARIO_ID) VALUES (321, 'REALIZADA INCORRECTAMENTE', 'reparto de alimentos a distintos puntos', 'tres días', 16, 'Actriz')
-INSERT INTO APP.USUARIO  (USUARIO, APELLIDOS, CONTRASENIA, DNI, EMAIL, ESTUDIOS, FECHA_NACIMIENTO, IDIOMA, NOMBRE, ROL) VALUES ('Detective', 'Holmes', 'pass12341', 77453728, 'usuario1@hotmail.com', 'psicologia', '1800-11-30','ingles', 'Sherlock', 'NORMAL')
-INSERT INTO APP.USUARIO  VALUES('usuario1','usuario1','usuario1',1111111,'usuario1','2020-01-12','usuario1','usuario1','ALUMNO')
-INSERT INTO APP.USUARIO (USUARIO,APELLIDOS,CRONTASENIA,DNI,EMAIL,ESTUDIOS,FECHA_NACIMIENTO,IDIOMA,NOMBRE,ROL) VALUES('usuario2','usuario1','usuario1',1111112,'usuario1','2020-01-12','usuario1','usuario2','ALUMNO')
-INSERT INTO APP.USUARIO (USUARIO,APELLIDOS,CRONTASENIA,DNI,EMAIL,ESTUDIOS,FECHA_NACIMIENTO,IDIOMA,NOMBRE,ROL) VALUES('usuario3','usuario1','usuario1',1111113,'usuario1','2020-01-12','usuario1','usuario3','ALUMNO')
-INSERT INTO APP.USUARIO (USUARIO,APELLIDOS,CRONTASENIA,DNI,EMAIL,ESTUDIOS,FECHA_NACIMIENTO,IDIOMA,NOMBRE,ROL) VALUES('usuario4','usuario1','usuario1',1111114,'usuario1','2020-01-12','usuario1','usuario4','RESPONSABLE')
-INSERT INTO APP.USUARIO (USUARIO,APELLIDOS,CRONTASENIA,DNI,EMAIL,ESTUDIOS,FECHA_NACIMIENTO,IDIOMA,NOMBRE,ROL) VALUES('usuario5','usuario1','usuario1',1111115,'usuario1','2020-01-12','usuario1','usuario5','RESPONSABLE')
-INSERT INTO APP.USUARIO (USUARIO,APELLIDOS,CRONTASENIA,DNI,EMAIL,ESTUDIOS,FECHA_NACIMIENTO,IDIOMA,NOMBRE,ROL) VALUES('usuario6','usuario1','usuario1',1111116,'usuario1','2020-01-12','usuario1','usuario6','RESPONSABLE')
-INSERT INTO APP.USUARIO (USUARIO,APELLIDOS,CRONTASENIA,DNI,EMAIL,ESTUDIOS,FECHA_NACIMIENTO,IDIOMA,NOMBRE,ROL) VALUES('usuario7','usuario1','usuario1',1111117,'usuario1','2020-01-12','usuario1','usuario7','ONG')
-INSERT INTO APP.USUARIO (USUARIO,APELLIDOS,CRONTASENIA,DNI,EMAIL,ESTUDIOS,FECHA_NACIMIENTO,IDIOMA,NOMBRE,ROL) VALUES('usuario8','usuario1','usuario1',11111118,'usuario1','2020-01-12','usuario1','usuario8','ONG')
-INSERT INTO APP.USUARIO (USUARIO,APELLIDOS,CRONTASENIA,DNI,EMAIL,ESTUDIOS,FECHA_NACIMIENTO,IDIOMA,NOMBRE,ROL) VALUES('usuario9','usuario1','usuario1',11111119,'usuario1','2020-01-12','usuario1','usuario9','ONG')
-INSERT INTO APP.USUARIO (USUARIO,APELLIDOS,CRONTASENIA,DNI,EMAIL,ESTUDIOS,FECHA_NACIMIENTO,IDIOMA,NOMBRE,ROL) VALUES('usuario10','usuario1','usuario1',111119,'usuario1','2020-01-12','usuario1','usuario7','NORMAL')
-INSERT INTO APP.USUARIO (USUARIO,APELLIDOS,CRONTASENIA,DNI,EMAIL,ESTUDIOS,FECHA_NACIMIENTO,IDIOMA,NOMBRE,ROL) VALUES('usuario11','usuario1','usuario1',111115,'usuario1','2020-01-12','usuario1','usuario8','NORMAL')
-INSERT INTO APP.USUARIO (USUARIO,APELLIDOS,CRONTASENIA,DNI,EMAIL,ESTUDIOS,FECHA_NACIMIENTO,IDIOMA,NOMBRE,ROL) VALUES('usuario12','usuario1','usuario1',11117,'usuario1','2020-01-12','usuario1','usuario9','NORMAL')
-INSERT INTO APP.ALUMNO VALUES('usuario1',12345,'informatica')
-INSERT INTO APP.ALUMNO VALUES('usuario2',123456,'informatica')
-INSERT INTO APP.ALUMNO VALUES('usuario3',1234567,'informatica')
-INSERT INTO APP.RESPONSABLE_ACTIVIDAD VALUES('usuario4','LCC')
-INSERT INTO APP.RESPONSABLE_ACTIVIDAD VALUES('usuario5','LCC')
-INSERT INTO APP.RESPONSABLE_ACTIVIDAD VALUES('usuario6','LCC')
-INSERT INTO APP.ONG VALUES('usuario7','esto es la descripcion','espana',777888)
-INSERT INTO APP.ONG VALUES('usuario8','esto es la descripcion','espana',777888)
-INSERT INTO APP.ONG VALUES('usuario9','esto es la descripcion','espana',777888)
+
+INSERT INTO APP.ONG (USUARIO,CONTRASENIA,DESCRIPCION,DIRECCION,DNI, EMAIL, FECHA_NACIMIENTO,NOMBRE,PAIS, ROL,TELEFONO) VALUES ('ong2', '1234','ONG que trata de repartir recursos','Calle Quevedo',38504,'dosong@gmail.com','2004-03-19', 'Todos Tienen',  'España','ONG', 623630415)
+
+INSERT INTO APP.ONG (USUARIO,CONTRASENIA, DESCRIPCION,DIRECCION,DNI, EMAIL, FECHA_NACIMIENTO,NOMBRE,PAIS, ROL,TELEFONO) VALUES ('ong3', '1234','ONG que trata de salvar el medio ambiente','Calle Picos de Europa',20572,'arbolesverdes@gmail.com','1999-08-15', 'Arboles verdes',  'España','ONG', 608321654)
+
 
 INSERT INTO APP.ACTIVIDAD VALUES(2,'Comedor solidario','PENDIENTE','2020-06-13','2020-07-13','HUELVA', 'COMEDOR','VOLUNTARIADO','ong2','resp')
 
